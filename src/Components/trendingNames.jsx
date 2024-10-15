cae8771a0a4fadb046f1a747eb539af206e9f2d5
aisha <aisharaja123@icloud.com>
2024-10-16 01:04:35 +0500

    Routes Configured

---
+++ src/Components/trendingNames.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TrendingNames = () => {
+  return (
+    <div>
+      <h1>Trending Names Page</h1>
+      {/* Add your people search functionality here */}
+    </div>
+  );
+};
+
+export default TrendingNames;
